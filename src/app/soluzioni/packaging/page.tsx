55df1d9e13d933a283942b33375640fe00184b0c
Jarvis <jarvis@printsolutionsrl.it>
2026-02-12 12:50:37 +0100

    Packaging: EDM-650X usa immagine scontornata

--- src/app/soluzioni/packaging/page.tsx
+++ src/app/soluzioni/packaging/page.tsx
@@ -64,7 +64,7 @@ const products = [
     ],
     gradient: "from-magenta-500 to-magenta-600",
     icon: "🖨️",
-    image: "/images/products/edm-650x-photo.avif",
+    image: "/images/products/edm-650x-2hd-nobg.png",
     href: "/prodotti/edm-650x",
     brand: "Print Solution",
     brandHref: null,
